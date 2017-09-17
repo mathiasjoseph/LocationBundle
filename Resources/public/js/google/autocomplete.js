8c932395e087ed517530c73d775d0ff74a67913d
miky <mathias89100@gmail.com>
2017-09-17 19:52:54 +0200

    Merge branch 'master' of github.com:mikyprog/Order
    # Conflicts:
    #	.gitignore

--- Resources/public/js/google/autocomplete.js
+++ Resources/public/js/google/autocomplete.js
@@ -1,7 +1,7 @@
 var map;
 var marker = new google.maps.Marker();
 function initializeAutocomplete(id) {
-    var element = document.getElementById(id);
+    var element = $("."+id)[0];
     if (element) {
         var autocomplete = new google.maps.places.Autocomplete(element, {types: ['geocode']});
         google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
@@ -9,52 +9,54 @@ function initializeAutocomplete(id) {
 }
 
 function initializeMap() {
-    var mapCanvas = document.getElementById("map");
-    if (document.getElementById("lat").value != null) {
-        var mapOptions = {
-            center: new google.maps.LatLng(document.getElementById("lat").value, document.getElementById("lng").value),
-            zoom: 16
+    var lat_element = $(".lat")[0];
+    var lng_element = $(".lng")[0];
+    if($(".map").length) {
+        var mapCanvas = $(".map")[0];
+        if (lat_element.value != null) {
+            var mapOptions = {
+                center: new google.maps.LatLng(lat_element.value, lng_element.value),
+                zoom: 5
+            }
+            map = new google.maps.Map(mapCanvas, mapOptions);
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(lat_element, lng_element),
+                map: map
+            });
+        } else {
+            var mapOptions = {
+                center: new google.maps.LatLng(1, 1),
+                zoom: 5
+            }
+            map = new google.maps.Map(mapCanvas, mapOptions);
         }
-        map = new google.maps.Map(mapCanvas, mapOptions);
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(document.getElementById("lat"), document.getElementById("lng")),
-            map: map
-        });
-    } else {
-        var mapOptions = {
-            center: new google.maps.LatLng(1, 1),
-            zoom: 16
-        }
-        map = new google.maps.Map(mapCanvas, mapOptions);
     }
-
 }
 function onPlaceChanged() {
     var place = this.getPlace();
-
+    var lat_element = $(".lat")[0];
+    var lng_element = $(".lng")[0];
     var lat = place.geometry.location.lat();
     var lng = place.geometry.location.lng();
-
-    if (lng != null && lat != null) {
+    if($(".map").length){
+        if (lng != null && lat != null) {
         map.setCenter(new google.maps.LatLng(lat, lng));
         marker.setMap(null);
         marker = new google.maps.Marker({
             position: new google.maps.LatLng(lat, lng),
             map: map
         });
-        document.getElementById("lat").value = lat;
-        document.getElementById("lng").value = lng;
-    }
+        $(".lat")[0].value = lat;
+        $(".lng")[0] .value = lng;
+    }}
     for (var i in place.address_components) {
         var component = place.address_components[i];
-        for (var j in component.types) {  // Some types are ["country", "political"]
-            var type_element = document.getElementById(component.types[j]);
-            if (type_element) {
-                type_element.value = component.long_name;
+        for (var j in component.types) {
+            if ($("."+component.types[j]).length) {
+                $("."+component.types[j]).val(component.long_name);
             }
-            var type_element_short = document.getElementById(component.types[j] + "_short");
-            if (type_element_short) {
-                type_element_short.value = component.short_name;
+            if ($("."+component.types[j]+ "_short").length) {
+                $("."+component.types[j]+ "_short").val(component.short_name);
             }
         }
     }
@@ -63,6 +65,6 @@ function onPlaceChanged() {
 
 google.maps.event.addDomListener(window, 'load', function () {
     initializeMap();
-    initializeAutocomplete('user_input_autocomplete_address');
+    initializeAutocomplete('gm_autocomplete_address');
 });
 
